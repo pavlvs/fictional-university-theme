6a05eaadb809c226acef33287d9c81d3099300c8
Paul Shaw <pavlvsxavier@gmail.com>
2020-08-24 19:37:25 +0100

    created index, page and single page with the loop

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-Foo reigns supreme, mylord.
+<?php
+
+while (have_posts()) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a> </h2> <?php the_content(); ?>
+    <hr>
+<?php }
+
+?>
