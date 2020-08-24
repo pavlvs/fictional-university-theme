144733b872b2ef8b29f7698b89f008923f276dba
Paul Shaw <pavlvsxavier@gmail.com>
2020-08-24 20:31:56 +0100

    added header and footer

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+get_header();
 
 while (have_posts()) {
     the_post(); ?>
@@ -6,5 +7,6 @@ while (have_posts()) {
     <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
 <?php }
+get_footer();
 
 ?>
