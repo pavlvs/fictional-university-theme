20ac18e01e56ce008445ceb3db9f9f060587fd22
Paul Shaw <pavlvsxavier@gmail.com>
2020-08-25 16:36:22 +0100

    interior pages, parent & children page,s child pages menu links

--- functions.php
+++ functions.php
@@ -9,4 +9,11 @@ function university_files()
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'university_files'); ?>
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features()
+{
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
