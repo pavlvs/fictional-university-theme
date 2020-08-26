ee68cba8ee81b41119ee2a8cd15ee7966442f96f
Paul Shaw <pavlvsxavier@gmail.com>
2020-08-26 11:17:21 +0100

    completed blog page with archives

--- archive.php
+++ archive.php
@@ -3,9 +3,14 @@ get_header(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome to our blog!</h1>
+        <h1 class="page-banner__title">
+            <?php if (is_category()) : single_cat_title() ?>
+            <?php elseif (is_author()) : echo 'Posts by ';
+                the_author() ?>
+            <?php endif; ?>
+        </h1>
         <div class="page-banner__intro">
-            <p$this->Keep up with our latest news.</p>
+            <p><?php the_archive_description(); ?></p>
         </div>
     </div>
 </div>
