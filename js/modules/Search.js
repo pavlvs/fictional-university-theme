84130eb88e679701c4e18ee0763d45f4ca56c674
Paul J. Shaw <pavlvsxavier@gmail.com>
2020-09-04 15:45:55 +0100

    added a timer to the search input typing

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -6,8 +6,10 @@ class Search {
         this.openButton = $('.js-search-trigger');
         this.closeButton = $('.search-overlay__close');
         this.searchOverlay = $('.search-overlay');
+        this.searchField = $('#search-term');
         this.events();
         this.isOverlayOpen = false;
+        this.typingTimer;
     }
 
     // 2. event handlers
@@ -15,6 +17,7 @@ class Search {
         this.openButton.on('click', this.openOverlay.bind(this));
         this.closeButton.on('click', this.closeOverlay.bind(this));
         $(document).on('keydown', this.keyPressDispatcher.bind(this));
+        this.searchField.on('keydown', this.typingLogic.bind(this));
     }
 
     //3. methods
@@ -37,6 +40,13 @@ class Search {
         if (e.keyCode == 27 && this.isOverlayOpen) {
             this.closeOverlay();
         }
+    }
+
+    typingLogic() {
+        clearTimeout(this.typingTimer);
+        this.typingTimer = setTimeout(() => {
+            console.log('mylord?');
+        }, 2000);
     }
 }
 
