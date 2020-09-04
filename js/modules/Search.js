1ab5fe436b11358a560f7bdaf10f5705e856a8aa
Paul J. Shaw <pavlvsxavier@gmail.com>
2020-09-05 00:50:12 +0100

    moved modal to search.js

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,50 +1,30 @@
-import $ from 'jQuery';
+import $ from 'jquery';
 
 class Search {
-    // 1. describe and create object
+    // 1. describe and create/initiate our object
     constructor() {
+        this.addSearchHTML();
+        this.resultsDiv = $('#search-overlay__results');
+        this.openButton = $('.js-search-trigger');
         this.closeButton = $('.search-overlay__close');
+        this.searchOverlay = $('.search-overlay');
+        this.searchField = $('#search-term');
         this.events();
         this.isOverlayOpen = false;
         this.isSpinnerVisible = false;
-        this.openButton = $('.js-search-trigger');
         this.previousValue;
-        this.resultsDiv = $('#search-overlay__results');
-        this.searchField = $('#search-term');
-        this.searchOverlay = $('.search-overlay');
         this.typingTimer;
     }
 
-    // 2. event handlers
+    // 2. events
     events() {
         this.openButton.on('click', this.openOverlay.bind(this));
         this.closeButton.on('click', this.closeOverlay.bind(this));
         $(document).on('keydown', this.keyPressDispatcher.bind(this));
-        this.searchField.on('keydown', this.typingLogic.bind(this));
+        this.searchField.on('keyup', this.typingLogic.bind(this));
     }
 
-    //3. methods
-    openOverlay() {
-        this.searchOverlay.addClass('search-overlay--active');
-        $('body').addClass('body-no-scroll');
-        this.isOverlayOpen = true;
-    }
-
-    closeOverlay() {
-        this.searchOverlay.removeClass('search-overlay--active');
-        $('body').removeClass('body-no-scroll');
-        this.isOverlayOpen = false;
-    }
-
-    keyPressDispatcher(e) {
-        if (e.keyCode == 83 && !this.isOverlayOpen) {
-            this.openOverlay();
-        }
-        if (e.keyCode == 27 && this.isOverlayOpen) {
-            this.closeOverlay();
-        }
-    }
-
+    // 3. methods (function, action...)
     typingLogic() {
         if (this.searchField.val() != this.previousValue) {
             clearTimeout(this.typingTimer);
@@ -54,7 +34,7 @@ class Search {
                     this.resultsDiv.html('<div class="spinner-loader"></div>');
                     this.isSpinnerVisible = true;
                 }
-                this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+                this.typingTimer = setTimeout(this.getResults.bind(this), 750);
             } else {
                 this.resultsDiv.html('');
                 this.isSpinnerVisible = false;
@@ -65,6 +45,8 @@ class Search {
     }
 
     getResults() {
+        console.log(this.isSpinnerVisible);
+        console.log('mylord!');
         $.getJSON(
             universityData.root_url +
                 '/wp-json/wp/v2/posts?search=' +
@@ -88,6 +70,50 @@ class Search {
                 this.isSpinnerVisible = false;
             }
         );
+    }
+
+    keyPressDispatcher(e) {
+        if (
+            e.keyCode == 83 &&
+            !this.isOverlayOpen &&
+            !$('input, textarea').is(':focus')
+        ) {
+            this.openOverlay();
+        }
+        if (e.keyCode == 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
+    }
+
+    openOverlay() {
+        this.searchOverlay.addClass('search-overlay--active');
+        $('body').addClass('body-no-scroll');
+        console.log('our open method just ran!');
+        this.isOverlayOpen = true;
+    }
+
+    closeOverlay() {
+        this.searchOverlay.removeClass('search-overlay--active');
+        $('body').removeClass('body-no-scroll');
+        console.log('our close method just ran!');
+        this.isOverlayOpen = false;
+    }
+
+    addSearchHTML() {
+        $('body').append(`
+            <div class="search-overlay">
+                <div class="search-overlay__top">
+                    <div class="container">
+                        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                        <input type="text" id="search-term" placeholder="What are you looking for?" class="search-term" autocomplete="off">
+                        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div class="container">
+                    <div id="search-overlay__results"></div>
+                </div>
+            </div>
+        `);
     }
 }
 
